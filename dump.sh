bd8277ef45cc999f117005024a36f3ff5eb4973c
Rodrigo Avila <rodrigo.avila@mailtop.com.br>
2018-06-28 09:43:43 -0300

    Direct dump to bzip2

--- dump.sh
+++ dump.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 set -e
 
-FILE="$(date +%Y%m%d%H%M%S)-$POSTGRES_DB.out"
+FILE="$(date +%Y%m%d%H%M%S)-$POSTGRES_DB.out.bz2"
 
 echo "Creating file $FILE..."
 PGPASSWORD="$POSTGRES_PASSWORD" pg_dump -U $POSTGRES_USER \
                                         -h $POSTGRES_HOST \
-                                        -d $POSTGRES_DB > $FILE
-
-echo "Compressing to $FILE.bz2..."
-bzip2 -9 $FILE
+                                        -d $POSTGRES_DB | bzip2 -c > $FILE
 
 echo "Uploading to $AWS_S3_BUCKET..."
 aws s3 cp $FILE.bz2 $AWS_S3_BUCKET/$FILE.bz2
